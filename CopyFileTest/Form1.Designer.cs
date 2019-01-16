0e88aed2977439d67b02a37347bb8d71537e22cf
a1574944154 <a1574944154@gmail.com>
2019-01-16 10:12:37 +0800

    first commit

---
+++ CopyFileTest/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace CopyFileTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_fromdir = new System.Windows.Forms.Button();
+            this.btn_todir = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "选择文件夹";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(425, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "路径";
+            // 
+            // btn_fromdir
+            // 
+            this.btn_fromdir.Location = new System.Drawing.Point(158, 33);
+            this.btn_fromdir.Name = "btn_fromdir";
+            this.btn_fromdir.Size = new System.Drawing.Size(174, 48);
+            this.btn_fromdir.TabIndex = 3;
+            this.btn_fromdir.Text = "点击";
+            this.btn_fromdir.UseVisualStyleBackColor = true;
+            this.btn_fromdir.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_todir
+            // 
+            this.btn_todir.Location = new System.Drawing.Point(480, 33);
+            this.btn_todir.Name = "btn_todir";
+            this.btn_todir.Size = new System.Drawing.Size(174, 48);
+            this.btn_todir.TabIndex = 3;
+            this.btn_todir.Text = "点击";
+            this.btn_todir.UseVisualStyleBackColor = true;
+            this.btn_todir.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "单线程";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(74, 299);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "多线程";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(175, 175);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(312, 40);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(175, 285);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(312, 40);
+            this.progressBar2.TabIndex = 6;
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(688, 217);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(100, 56);
+            this.btn_start.TabIndex = 7;
+            this.btn_start.Text = "开始复制";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(514, 199);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 15);
+            this.label5.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(514, 310);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 15);
+            this.label6.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(624, 187);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 15);
+            this.label7.TabIndex = 9;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(624, 299);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 15);
+            this.label8.TabIndex = 9;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_todir);
+            this.Controls.Add(this.btn_fromdir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_fromdir;
+        private System.Windows.Forms.Button btn_todir;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ CopyFileTest/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace CopyFileTest
+{
+    public delegate void UpdateProgressValueEvent();
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void updateProgress1Value()
+        {
+            progressBar1.Value++;
+            label5.Text = progressBar1.Value.ToString() + "/" + progressBar1.Maximum.ToString();
+        }
+
+        private void updateProgress2Value()
+        {
+            lock (progressBar2)
+            {
+                progressBar2.Value++;
+            }
+            label6.Text = progressBar2.Value.ToString() + "/" + progressBar2.Maximum.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件路径";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                btn_fromdir.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件路径";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                btn_todir.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            int fileNum = scanFileNum(btn_fromdir.Text);
+            progressBar1.Maximum = fileNum;
+            progressBar2.Maximum = fileNum;
+            progressBar1.Value = 0;
+            progressBar2.Value = 0;
+            sw.Restart();   //重置计时器
+
+            UpdateProgressValueEvent updateProValue1 = new UpdateProgressValueEvent(updateProgress1Value);
+            UpdateProgressValueEvent updateProValue2 = new UpdateProgressValueEvent(updateProgress2Value);
+
+            cst = new CopySingleThread(btn_fromdir.Text, (btn_todir.Text+"\\copySingle"));
+            cmt = new CopyMultiThread(btn_fromdir.Text, (btn_todir.Text + "\\copyMulti"));
+            cmt.OnUpdateProgressValue += new UpdateProgressValueEvent(updateProgress2Value);
+            cst.OnUpdateProgressValue += new UpdateProgressValueEvent(updateProgress1Value);
+
+            Thread thread0 = new Thread(new ThreadStart(cst.startCopy)); 
+            Thread thread1 = new Thread(new ThreadStart(cmt.startCopy));
+            thread0.IsBackground = true;
+            thread1.IsBackground = true;
+            thread0.Start();
+            thread1.Start();
+        }
+
+        public static int scanFileNum(string fileDir)
+        {
+            int num = 0;
+            string[] files = Directory.GetFiles(fileDir);
+            string[] fromDirs = Directory.GetDirectories(fileDir);
+            foreach (string fromDir in fromDirs)
+            {
+                num = num + scanFileNum(fromDir);
+            }
+            return num + files.Length;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sw = new Stopwatch();  //计时
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(progressBar1.Value!=progressBar1.Maximum)
+            {
+                label7.Text = sw.Elapsed.ToString().Split(':')[2];
+            }
+            if(progressBar2.Value!=progressBar2.Maximum)
+            {
+                label8.Text = sw.Elapsed.ToString().Split(':')[2];
+            }
+        }
+
+        private Stopwatch sw;  //计时
+        private CopyMultiThread cmt;
+        private CopySingleThread cst;
+    }
+
+    public class CopyMultiThread
+    {
+        public event UpdateProgressValueEvent OnUpdateProgressValue;
+
+        public CopyMultiThread(string fromDir, string toDir)
+        {
+            IsScanComplete = false;
+            this.fromDir = fromDir;
+            this.toDir = toDir;
+            Tasks = new ConcurrentQueue<string[]>();
+
+        }
+
+        public void startCopy()
+        {
+            Thread thread0 = new Thread(new ThreadStart(createDirThenEnqueue));
+            thread0.IsBackground = true;
+            thread0.Start();
+
+            for(int i=0;i<20;i++)
+            {
+                ThreadPool.QueueUserWorkItem(copyFileTask);
+            }
+        }
+
+        private void createDirThenEnqueue()
+        {
+            createDirThenEnqueue(fromDir, toDir);
+            IsScanComplete = true;
+        }
+
+        private void createDirThenEnqueue(string fromDir, string toDir)
+        {
+            if(!Directory.Exists(toDir))
+            {
+                Directory.CreateDirectory(toDir);
+            }
+            string[] files = Directory.GetFiles(fromDir);
+
+            foreach(string file in files)
+            {
+                string filename = Path.GetFileName(file);
+                string toFilename = Path.Combine(toDir, filename);
+
+                string[] task = { file, toFilename };
+                Tasks.Enqueue(task);
+            }
+
+            string[] fromDirs = Directory.GetDirectories(fromDir);
+            foreach(string fromDirName in fromDirs)
+            {
+                string dirName = Path.GetFileName(fromDirName);
+                string toDirName = Path.Combine(toDir, dirName);
+                createDirThenEnqueue(fromDirName, toDirName);
+            }
+        }
+
+        private void copyFileTask(object state)
+        {
+            while(true)
+            {
+                if(Tasks.Count==0 && IsScanComplete==true)
+                {
+                    
+                    return;
+                }
+                string[] task;
+                bool isFalse = Tasks.TryDequeue(out task);
+                if(!isFalse)
+                {
+                    continue;
+                }
+                else
+                {
+                    File.Copy(task[0], task[1]);
+                    if(OnUpdateProgressValue!=null)
+                    {
+                        OnUpdateProgressValue();
+                    }
+                }
+            }
+        }
+
+        private string fromDir;
+        private string toDir;
+        private bool IsScanComplete;
+        private ConcurrentQueue<string[]> Tasks;
+    }
+
+    public class CopySingleThread
+    {
+        public event UpdateProgressValueEvent OnUpdateProgressValue;
+
+        public CopySingleThread(string fromDir, string toDir)
+        {
+            this.fromDir = fromDir;
+            this.toDir = toDir;
+        }
+
+        public void startCopy()
+        {
+            copyRecursion(fromDir, toDir);
+        }
+
+        private void copyRecursion(string fromDir, string toDir)  //递归复制
+        {
+            if (!Directory.Exists(toDir))
+            {
+                Directory.CreateDirectory(toDir);
+            }
+
+            string[] files = Directory.GetFiles(fromDir);
+            foreach (string file in files)
+            {
+                string filename = Path.GetFileName(file);
+                string toFilename = Path.Combine(toDir, filename);
+                File.Copy(file, toFilename);
+                if(OnUpdateProgressValue!=null)
+                {
+                    OnUpdateProgressValue();
+                }
+                
+            }
+            string[] fromDirs = Directory.GetDirectories(fromDir);
+            foreach (string fromDirName in fromDirs)
+            {
+                string dirName = Path.GetFileName(fromDirName);
+                string toDirName = Path.Combine(toDir, dirName);
+                copyRecursion(fromDirName, toDirName);
+            }
+        }
+
+        private string fromDir;
+        private string toDir;
+    }
+}
